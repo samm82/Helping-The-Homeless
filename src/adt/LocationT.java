fe70138cd5a5a6e56168069b9bb4b96c5033daaf
unknown <marin@mcmaster.ca>
2019-03-12 16:23:07 -0400

    Updated ADT LocationT.java

--- src/adt/LocationT.java
+++ src/adt/LocationT.java
@@ -1,5 +1,29 @@
 package adt;
 
 public class LocationT {
-
+	
+	private String name, address;
+	private float score;
+	
+	public LocationT(String name, String address) {
+		this.name = name;
+		this.address = address;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getAddress() {
+		return this.address;
+	}
+	
+	public float getScore() {
+		return this.score;
+	}
+	
+	public void setScore(float nScore) {
+		this.score = nScore;
+	}
+	
 }
