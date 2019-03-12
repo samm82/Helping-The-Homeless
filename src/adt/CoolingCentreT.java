5c791a08fe41fe6c453715a846e8c083ea2dd30d
Sam Crawford <sam.crawford82@gmail.com>
2019-03-12 16:51:53 -0400

    Implemented CoolingCentreT

--- src/adt/CoolingCentreT.java
+++ src/adt/CoolingCentreT.java
@@ -1,5 +1,18 @@
 package adt;
 
 public class CoolingCentreT extends LocationT {
+	
+	private enum CentreT { // might not end up using
+		LIBRARY,
+		COMM_CNTR
+	}
+	
+	private String name, address;
+	private CentreT type;
+	
+	public CoolingCentreT(CentreT type, String name, String address) {
+		super(name, address);
+		this.type = type;
+	}
 
 }
