eac649544f754d9da15beabf6de690f58dfe93aa
unknown <marin@mcmaster.ca>
2019-03-12 16:59:46 -0400

    Updated ADT ShelterT.java

--- src/adt/ShelterT.java
+++ src/adt/ShelterT.java
@@ -14,27 +14,49 @@ public class ShelterT extends LocationT {
 	
 	private ResidentT type;
 	private String name, address;
-	private int occupancy, capacity;
+	private int[] occupancy2018, capacity2018, occupancy2017, capacity2017;
+	private int N;
 	
-	public ShelterT(ResidentT type, String name, String address, int occupancy, int capacity) {
+	public ShelterT(ResidentT type, String name, String address) {
 		super(name, address);
 		this.type = type;
-		this.occupancy = occupancy;
-		this.capacity = capacity;
+		occupancy2018 = new int[365];
+		capacity2018 = new int[365];
+		occupancy2017 = new int[365];
+		capacity2017 = new int[365];
+		N = 0;
 	}
 	
-	
-	public boolean isValidType(UserT user) {
-		GenderT gen  = user.getGender();
-		
-		if (user.isFamily()) return (type == ResidentT.FAMILY);
-		if (user.isYouth())  return (type == ResidentT.YOUTH);
-		
-		if      (user.isFlexible()     && type == ResidentT.COED  ) return true;
-		else if (gen == GenderT.MALE   && type == ResidentT.MALE  ) return true;
-		else if (gen == GenderT.FEMALE && type == ResidentT.FEMALE) return true;
-		else                                                        return false;
-		
+	public void setOccupancy(int occupancy, int year) {
+		if(year == 2018) {
+			this.occupancy2018[N] = occupancy;
+		}
+		if(year == 2017) {
+			this.occupancy2017[N] = occupancy;
+		}
 	}
+	
+	public void setCapacity(int capacity, int year) {
+		if(year == 2018) {
+			this.capacity2018[N] = capacity;
+		}
+		if(year == 2017) {
+			this.capacity2017[N] = capacity;
+		}
+	}
+	
+//  May not be used due to changing the implementation of user type	
+//	public boolean isValidType(UserT user) {
+//		GenderT gen  = user.getGender();
+//		
+//		if (user.isFamily()) return (type == ResidentT.FAMILY);
+//		if (user.isYouth())  return (type == ResidentT.YOUTH);
+//		
+//		if      (user.isFlexible()     && type == ResidentT.COED  ) return true;
+//		else if (gen == GenderT.MALE   && type == ResidentT.MALE  ) return true;
+//		else if (gen == GenderT.FEMALE && type == ResidentT.FEMALE) return true;
+//		else                                                        return false;
+//		
+//	}
 
 }
