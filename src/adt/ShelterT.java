ef1b91bcc76a9d8a15c50c19a2dc12390ead6a0c
Sam Crawford <sam.crawford82@gmail.com>
2019-03-12 15:38:38 -0400

    Started implementing isValidType

--- src/adt/ShelterT.java
+++ src/adt/ShelterT.java
@@ -1,5 +1,28 @@
 package adt;
 
+import adt.UserT.GenderT;
+
 public class ShelterT extends LocationT {
+	
+	enum ResidentT {
+		MALE,
+		FEMALE,
+		COED,
+		FAMILY,
+		YOUTH
+	}
+	
+	private ResidentT type;
+	
+	public boolean isValidType(UserT user) {
+		GenderT gen  = user.getGender();
+		boolean flex = user.getFlexibility();
+		
+		if (flex                  && type == ResidentT.COED  ) return true;
+		if (gen == GenderT.MALE   && type == ResidentT.MALE  ) return true;
+		if (gen == GenderT.FEMALE && type == ResidentT.FEMALE) return true;
+		else                                                   return false;
+		
+	}
 
 }
--- src/adt/UserT.java
+++ src/adt/UserT.java
@@ -1,5 +1,16 @@
 package adt;
 
 public class UserT {
-
+	
+	enum GenderT {
+		MALE,
+		FEMALE
+	}
+	
+	private GenderT gender;
+	private boolean flexible; // OK with being with other gender
+	
+	public GenderT getGender()      { return gender; }
+	public boolean getFlexibility() { return flexible; }
+	
 }
