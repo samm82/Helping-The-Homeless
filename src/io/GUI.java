dfc306907f4ab19a21271a43bf520a062544b0d9
Sam Crawford <sam.crawford82@gmail.com>
2019-03-14 14:05:07 -0400

    Fixed imports in GUI (again)

--- src/io/GUI.java
+++ src/io/GUI.java
@@ -1,24 +1,18 @@
 package io;
 
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.wb.swt.SWTResourceManager;
-
-import java.util.ArrayList;
-
-import javax.swing.JScrollPane;
-
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.List;
-import org.eclipse.swt.widgets.ScrollBar;
-import org.eclipse.swt.custom.ScrolledComposite;
-import org.eclipse.swt.widgets.Slider;
-import org.eclipse.swt.widgets.Text;
-import org.eclipse.swt.widgets.Button;
+
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+import org.eclipse.wb.swt.SWTResourceManager;
 
 public class GUI {
 
