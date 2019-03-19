ffdacc58a26be0c36fddbb9dc9db3c788505f5e2
Joshua Guinness <joshua.guinness@gmail.com>
2019-03-19 16:18:17 -0400

    Added a comment to the Sort module and moved the MaxPQ module

--- src/process/MaxPQ.java
+++ src/process/MaxPQ.java
@@ -4,7 +4,7 @@
  * https://algs4.cs.princeton.edu/code/edu/princeton/cs/algs4/MaxPQ.java.html
  */
 
-package control;
+package process;
 
 import java.util.Comparator;
 import java.util.Iterator;
--- src/process/Sort.java
+++ src/process/Sort.java
@@ -1,5 +1,6 @@
 package process;
 
+// Module which will make the array of shelters with assigned scores into a priority queue
 public class Sort {
 
 }
