f3c6064b1de6a3e290de0421af8eeb1b3727db24
Jasim <jasima@mcmaster.ca>
2019-03-19 15:59:45 -0400

    Finished GUI

--- src/io/GUI.java
+++ src/io/GUI.java
@@ -18,6 +18,15 @@ public class GUI {
 
 	protected Shell shell;
 	private Text text;
+	private boolean female;
+	private boolean male;
+	private boolean youth;
+	private boolean family;
+	private boolean neither;
+	private boolean CoEd;
+	private boolean noCoEd;
+	
+	
 
 	/**
 	 * Launch the application.
@@ -77,12 +86,21 @@ public class GUI {
 		Female.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
+				female = true;
+				male = false;
 			}
 		});
 		Female.setBounds(116, 10, 72, 20);
 		Female.setText("Female");
 		
 		Button Male = new Button(group, SWT.RADIO);
+		Male.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				male = true;
+				female = false;
+			}
+		});
 		Male.setBounds(0, 10, 57, 20);
 		Male.setText("Male");
 		
@@ -91,10 +109,24 @@ public class GUI {
 		group_1.setBounds(426, 76, 214, 33);
 		
 		Button btnNoCoed = new Button(group_1, SWT.RADIO);
+		btnNoCoed.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				CoEd = false;
+				noCoEd = true;
+			}
+		});
 		btnNoCoed.setBounds(122, 10, 89, 20);
 		btnNoCoed.setText("No Co-ed");
 		
 		Button btnCoed = new Button(group_1, SWT.RADIO);
+		btnCoed.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				CoEd = true;
+				noCoEd = false;
+			}
+		});
 		btnCoed.setBounds(0, 10, 72, 20);
 		btnCoed.setText("Co-ed");
 		
@@ -103,14 +135,38 @@ public class GUI {
 		group_2.setBounds(210, 114, 242, 32);
 		
 		Button btnNeither = new Button(group_2, SWT.RADIO);
+		btnNeither.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				youth = false;
+				family = false;
+				neither = true;
+			}
+		});
 		btnNeither.setBounds(170, 10, 72, 20);
 		btnNeither.setText("Neither");
 		
 		Button btnFamily = new Button(group_2, SWT.RADIO);
+		btnFamily.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				youth = false;
+				family = true;
+				neither = false;
+			}
+		});
 		btnFamily.setBounds(82, 10, 66, 20);
 		btnFamily.setText("Family");
 		
 		Button btnYouth = new Button(group_2, SWT.RADIO);
+		btnYouth.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				youth = true;
+				family = false;
+				neither = false;
+			}
+		});
 		btnYouth.setBounds(0, 10, 66, 20);
 		btnYouth.setText("Youth");
 		
@@ -118,12 +174,14 @@ public class GUI {
 		btnFindShelter.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				for (int i = 1; i < 100; i++) {
-					int strnum = (int) (Math.sqrt(i*16)+Math.sqrt(i)+3*i);
-					float dist = (float) (i*1.25/3.5);
-					//Name, address, dist, type, 
-					text.append("Shelter " + i + "\t\t\t\t\t" + dist + "km" + "\n" + strnum + " Fake St W\n\n");
-				}
+				text.append("Male: "+ male + " Female: " + female + "\nCo-Ed: " + CoEd + " no Co-Ed: " + noCoEd + "\nYouth: " + youth + " Family: " + family + " Neither: " + neither);
+
+//				for (int i = 1; i < 100; i++) {
+//					int strnum = (int) (Math.sqrt(i*16)+Math.sqrt(i)+3*i);
+//					float dist = (float) (i*1.25/3.5);
+//					//Name, address, dist, type, 
+//					text.append("Shelter " + i + "\t\t\t\t\t" + dist + "km" + "\n" + strnum + " Fake St W\n\n");
+//				}
 			}
 		});
 		btnFindShelter.setBounds(10, 153, 630, 30);
