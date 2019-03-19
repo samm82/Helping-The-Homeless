fdc8063e8d0d938195427c2c9489f90bd3342cc1
unknown <marin@mcmaster.ca>
2019-03-19 16:57:32 -0400

    Updated read

--- src/io/Read.java
+++ src/io/Read.java
@@ -1,11 +1,16 @@
 package io;
 
+import java.io.FileReader;
 import java.util.ArrayList;
+
 import adt.ShelterT;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class Read {
-
-	public ShelterT[][] readShelterData() {
+	
+	public static ShelterT[][] readShelterData() {
 		ArrayList<ShelterT> maleArray = new ArrayList<ShelterT>();
 		ArrayList<ShelterT> femaleArray = new ArrayList<ShelterT>();
 		ArrayList<ShelterT> coedArray = new ArrayList<ShelterT>();
@@ -13,14 +18,26 @@ public class Read {
 		ArrayList<ShelterT> youthArray = new ArrayList<ShelterT>();
 		ShelterT[] var = new ShelterT[1];
 		
-		maleArray.add(new ShelterT( ShelterT.shelterResT.MALE, "Test", "123 Fun Street", 43.234786, -79.123983));
+		JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader("data/SMIS_Daily_Occupancy_2018.json"));
+            JSONArray jsonObject = (JSONArray) obj;
+            JSONObject arr = (JSONObject) jsonObject.get(0);
+            String arguments = (String) arr.get("SHELTER_NAME");
+            System.out.println(arguments);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 		
 		
 		ShelterT[][] temp = new ShelterT[5][100];
 		return temp;
 	}
 	
-	
+	public static void main(String[] args) {
+		readShelterData();
+	}
 	
 	
 }
