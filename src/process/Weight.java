b0d9684d9543c75d7be9a3e6bcfa019a6b4928ed
Sam Crawford <sam.crawford82@gmail.com>
2019-03-19 18:05:42 -0400

    Added weightDist as per #11

--- src/process/Weight.java
+++ src/process/Weight.java
@@ -1,5 +1,7 @@
 package process;
 
+import java.lang.IllegalArgumentException;
+
 import adt.ShelterT;
 import adt.UserT;
 
@@ -7,6 +9,21 @@ public class Weight {
 	
 	public static int calcCapacity(int cap17, int cap18) {
 		return (int) (cap17 * 0.3 + cap18 * 0.7);
+	}
+	
+	public static double weightDist(double dist) {
+		if      (dist < 0.0)  throw new IllegalArgumentException("Distance must be positive");
+		else if (dist < 0.5)  return 1.0;
+		else if (dist < 1.0)  return 0.9;
+		else if (dist < 1.5)  return 0.8;
+		else if (dist < 2.0)  return 0.7;
+		else if (dist < 3.0)  return 0.6;
+		else if (dist < 5.0)  return 0.5;
+		else if (dist < 7.0)  return 0.4;
+		else if (dist < 10.0) return 0.3;
+		else if (dist < 15.0) return 0.2;
+		else if (dist < 25.0) return 0.1;
+		else                  return 0.0;
 	}
 	
 	// Work in progress
