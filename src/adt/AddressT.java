57d16cdf23a7171d08a1aaf5df1e81ca6bcfd196
Sam Crawford <sam.crawford82@gmail.com>
2019-03-30 22:30:33 -0400

    Added AddressT adt

---
+++ src/adt/AddressT.java
@@ -0,0 +1,23 @@
+package adt;
+
+public class AddressT {
+	
+	private final int num;
+	private final String st;
+	private final double lat;
+	private final double lon;
+	
+	public AddressT(int num, String st, double lat, double lon) {
+		this.num = num;
+		this.st  = st;
+		this.lat = lat;
+		this.lon = lon;
+	}
+	
+	public int getNum()    { return num; }
+	public String getSt()  { return st;  }
+	public double getLat() { return lat; }
+	public double getLon() { return lon; }
+	
+
+}
