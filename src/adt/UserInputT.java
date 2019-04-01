1800505fa2088f417bc247460cbe349deaaf6148
Sam Crawford <sam.crawford82@gmail.com>
2019-04-01 17:59:51 -0400

    Finished UserInputT

--- src/adt/UserInputT.java
+++ src/adt/UserInputT.java
@@ -1,5 +1,18 @@
 package adt;
 
+import adt.UserT.UserResT;
+
 public class UserInputT {
+	
+	private UserResT type;
+	private String   add;
+	
+	public 	UserInputT(UserResT type, String add) {
+		this.type = type;
+		this.add  = add;
+	}
+	
+	public UserResT getType() { return this.type; }
+	public String   getAdd()  { return this.add;  }
 
 }
