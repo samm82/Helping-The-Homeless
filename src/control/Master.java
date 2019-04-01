7c953f329622a3b0a99a7da385ff22966bb44300
Sam Crawford <sam.crawford82@gmail.com>
2019-04-01 16:39:00 -0400

    Improved comments in Master

--- src/control/Master.java
+++ src/control/Master.java
@@ -1,57 +1,59 @@
 package control;
 
-import java.util.ArrayList;
-
-import algsstructs.*;
 import adt.AddressT;
 import adt.CoolingCentreT;
 import adt.ShelterT;
 import adt.UserT;
-import adt.ShelterT.shelterResT;
 import adt.UserT.UserResT;
+
+import algsstructs.*;
 import io.Read;
 import process.Weight;
 
 // Control module which will control all of our functionality
 public class Master {
 	
-	public static void main(String args[]) {		
-		System.out.println("Start");
+	public static void main(String args[]) {	
+		// creates a 2d array of all shelters
 		ShelterT[][] masterArray = Read.readShelterData();
-		System.out.println("Done");
-		//System.out.println(masterArray[1][7].getAddress());
 		
-		//System.out.println(Weight.calcScore(masterArray[0][4], new UserT(UserResT.MALE_ONLY, 43.239487, -73.109472)));
-		
-			
-		
-		// Prints all program names, scores, and total number of shelters
-//		int counter = 0;
-//		
-		
+		// creates a dummy user until we get it from the GUI
 		UserT user = new UserT(UserResT.MALE_ONLY, 43.239487, -73.109472);
+		
+		System.out.println();
+		System.out.println("Shelters:");
+		System.out.println();
 		
 		for (int i = 0; i < masterArray.length; i++) {
 			for (int j = 0; j < masterArray[i].length; j++) {
-				System.out.println(i + " " + j + " "  + masterArray[i][j].getProgName());
-				System.out.println("   " + Weight.calcScore(masterArray[i][j], user));
+				// sets score for each shelter
 				masterArray[i][j].setScore(Weight.calcScore(masterArray[i][j], user));
-//				counter ++;
+				
+				System.out.println(i + " " + j + " "  + masterArray[i][j].getProgName());
+				System.out.println("   " + masterArray[i][j].getScore());
 			}
 		}
 		
+		// creates a MaxPQ from each list of shelters by type
 		MaxPQ<ShelterT> pq0 = new MaxPQ<ShelterT>(masterArray[0]);
 		MaxPQ<ShelterT> pq1 = new MaxPQ<ShelterT>(masterArray[1]);
 		MaxPQ<ShelterT> pq2 = new MaxPQ<ShelterT>(masterArray[2]);
 		MaxPQ<ShelterT> pq3 = new MaxPQ<ShelterT>(masterArray[3]);
 		MaxPQ<ShelterT> pq4 = new MaxPQ<ShelterT>(masterArray[4]);
-//		
-//		System.out.println(counter);
 		
+		// creates a 1d array of all cooling centres
 		CoolingCentreT[] cool = Read.readCoolingData();
 		
+		System.out.println();
+		System.out.println("Cooling Centres:");
+		System.out.println();
+		
 		for (int i = 0; i < cool.length; i++) {
+			// sets score for each cooling centre
 			cool[i].setScore(Weight.calcScore(cool[i], user));
+			
+			System.out.println(i + " " + cool[i].getName());
+			System.out.println("   " + cool[i].getScore());
 		}
 		
 		MaxPQ<CoolingCentreT> coolPQ = new MaxPQ<CoolingCentreT>(cool);
