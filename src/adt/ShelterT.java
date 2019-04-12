0e82fef78a7ba53fb5c6ceb10175754223008719
Joshua Guinness <joshua.guinness@gmail.com>
2019-04-12 19:19:31 -0400

    Finished javadoc for the ADT's

--- src/adt/ShelterT.java
+++ src/adt/ShelterT.java
@@ -1,7 +1,17 @@
 package adt;
 
+/**
+ * ADT for a shelter, parent class is LocationT
+ * @author Sam and Nicholas
+ *
+ */
 public class ShelterT extends LocationT {
 	
+	/**
+	 * Enum for type of shelter
+	 * @author Sam
+	 *
+	 */
 	public enum shelterResT {
 		MALE, FEMALE, COED, FAMILY, YOUTH;
 	}
@@ -11,6 +21,15 @@ public class ShelterT extends LocationT {
 	private int[] occupancy2018, capacity2018, occupancy2017, capacity2017;
 	private int N, M;
 	
+	/**
+	 * Constructor for new ShelterT
+	 * @param type Shelter type
+	 * @param orgName Organization name
+	 * @param shelterName Shelter name
+	 * @param facilityName Facility name
+	 * @param progName Program name
+	 * @param address Address
+	 */
 	public ShelterT(shelterResT type, String orgName, String shelterName, String facilityName, String progName,  String address) {
 		super(shelterName, address, locTypeT.SHELTER);
 		this.type = type;
@@ -27,6 +46,12 @@ public class ShelterT extends LocationT {
 		M = 0;
 	}
 	
+	/**
+	 * Sets the occupancy and capacity
+	 * @param occupancy Sets the occupancy
+	 * @param capacity Sets the capacity
+	 * @param year Year data is from
+	 */
 	public void setCapOcc(int occupancy, int capacity, int year) {
 		if (year == 2018) {
 			this.occupancy2018[N] = occupancy;
@@ -40,10 +65,18 @@ public class ShelterT extends LocationT {
 		}
 	}
 	
+	/**
+	 * 
+	 * @return Type of shelter
+	 */
 	public shelterResT getType() {
 		return this.type;
 	}
 	
+	/**
+	 * 
+	 * @return Type of shelter in string form
+	 */
 	public String getTypeString() {
 		switch (getType()) {
 		case MALE:
@@ -62,35 +95,71 @@ public class ShelterT extends LocationT {
 		
 	}
 	
+	/**
+	 * 
+	 * @return Organization name
+	 */
 	public String getOrgName() {
 		return this.orgName;
 	}
 	
+	/**
+	 * 
+	 * @return Facility name
+	 */
 	public String getFacilityName() {
 		return this.facilityName;
 	}
 	
+	/**
+	 * 
+	 * @return Program name
+	 */
 	public String getProgName() {
 		return this.progName;
 	}
 	
+	/**
+	 * Gets the 2018 occupancy on a certain date
+	 * @param i Date index
+	 * @return Occupancy
+	 */
 	public int getOcc2018(int i) {
 		return this.occupancy2018[i];
 	}
 	
+	/**
+	 * Gets the 2017 occupancy on a certain date
+	 * @param i Date index
+	 * @return Occupancy
+	 */
 	public int getOcc2017(int i) {
 		return this.occupancy2017[i];
 	}
 	
+	/**
+	 * Gets the 2018 capacity on a certain date
+	 * @param i Date index
+	 * @return Capacity
+	 */
 	public int getCap2018(int i) {
 		return this.capacity2018[i];
 	}
 	
+	/**
+	 * Gets the 2017 capacity on a certain date
+	 * @param i Date index
+	 * @return Capacity
+	 */
 	public int getCap2017(int i) {
 		return this.capacity2017[i];
 	}
 	
-//  May not be used due to changing the implementation of user type	
+	/**
+	 * Returns true if the shelter is a possible match for the user based on shelter type
+	 * @param user The user
+	 * @return
+	 */
 	public boolean isValidType(UserT user) {	
 		if (user.isFamily()) return (type == shelterResT.FAMILY);
 		if (user.isYouth())  return (type == shelterResT.YOUTH);
@@ -102,14 +171,28 @@ public class ShelterT extends LocationT {
 		
 	}
 	
+	/**
+	 * Checks to see whether the shelter has a capacity greater than zero on a certain date
+	 * @param date Date index
+	 * @return Boolean
+	 */
 	public boolean isValidCap(int date) {
 		return !(this.getCap2018(date) == 0 | this.getCap2017(date) == 0);
 	}
 	
+	/**
+	 * Checks to see whether the shelter was full historically
+	 * @param date Date index
+	 * @return Boolean
+	 */
 	public boolean isFull(int date) {
 		return (this.getOcc2018(date) == this.getCap2018(date) && this.getOcc2017(date) == this.getCap2017(date));
 	}
 	
+	/**
+	 * Converts the shelter info to a string
+	 * @return String of shelter info
+	 */
 	@Override
 	public String toString() {
 		return this.getName() + ", " + this.getProgName() + " | " + this.getAddress();
--- src/adt/UserT.java
+++ src/adt/UserT.java
@@ -1,7 +1,17 @@
 package adt;
 
+/**
+ * ADT for a user
+ * @author Sam and Nicholas
+ *
+ */
 public class UserT {
 	
+	/**
+	 * Enum for user type
+	 * @author Sam
+	 *
+	 */
 	public enum UserResT {
 		MALE_ONLY,
 		MALE_COED,
@@ -14,28 +24,66 @@ public class UserT {
 	private UserResT resType;
 	private double lat, lon;
 	
+	/**
+	 * Constructor for new UserT
+	 * @param type User type
+	 * @param lat User latitude
+	 * @param lon User longitude
+	 */
 	public UserT(UserResT type, double lat, double lon) {
 		this.resType = type;
 		this.lat = lat;
 		this.lon = lon;
 	}
 	
+	/**
+	 * 
+	 * @return Type of user
+	 */
 	public UserResT getResType() { return resType; }
 	
+	/**
+	 * 
+	 * @return Boolean whether user is type family
+	 */
 	public boolean isFamily() { return (resType == UserResT.FAMILY); }
 	
+	/**
+	 * 
+	 * @return Boolean whether user is type youth
+	 */
 	public boolean isYouth() { return (resType == UserResT.YOUTH); }
 	
+	/**
+	 * 
+	 * @return Boolean whether user is open to co-ed shelters
+	 */
 	public boolean isFlexible() { return ((resType == UserResT.MALE_COED) || 
 										  (resType == UserResT.FEMALE_COED)); }
 	
+	/**
+	 * Boolean whether user type is male
+	 * @return
+	 */
 	public boolean isMale() { return ((resType == UserResT.MALE_COED) || 
 			  						  (resType == UserResT.MALE_ONLY)); }
 	
+	/**
+	 * 
+	 * @return Boolean whether user type is female
+	 */
 	public boolean isFemale() { return ((resType == UserResT.FEMALE_COED) || 
 			  							(resType == UserResT.FEMALE_ONLY)); }
 	
+	/** 
+	 * 
+	 * @return Latitude
+	 */
 	public double getLat() { return lat; }
 	
+	/**
+	 * 
+	 * @return Longitude
+	 */
 	public double getLon() { return lon; }
 }
