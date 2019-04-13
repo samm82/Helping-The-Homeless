9bb42e5a595b0b62cf175f736f16ebe4f5eaebf1
Joshua Guinness <joshua.guinness@gmail.com>
2019-04-12 22:35:21 -0400

    Added a test class suite for all the test cases

---
+++ src/adt/AllTests.java
@@ -0,0 +1,11 @@
+package adt;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AddressTTest.class, CoolingCentreTTest.class, ShelterTTest.class })
+public class AllTests {
+
+}
