4e1878634be5ed2ddac932279795002015057ae9
Jasim <jasima@mcmaster.ca>
2019-04-12 20:30:19 -0400

    Finished Cooling Center GUI

---
+++ src/io/CoolingCenter.java
@@ -0,0 +1,103 @@
+package io;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.wb.swt.SWTResourceManager;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.swt.widgets.Button;
+
+public class CoolingCenter {
+
+	protected Shell shell;
+	private Text address;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			CoolingCenter window = new CoolingCenter();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(511, 204);
+		shell.setText("SWT Application");
+		
+		address = new Text(shell, SWT.BORDER);
+		address.setBounds(86, 62, 393, 26);
+		
+		Label lblPleaseEnterYour = new Label(shell, SWT.NONE);
+		lblPleaseEnterYour.setAlignment(SWT.CENTER);
+		lblPleaseEnterYour.setFont(SWTResourceManager.getFont("Times New Roman", 14, SWT.BOLD));
+		lblPleaseEnterYour.setBounds(86, 10, 393, 30);
+		lblPleaseEnterYour.setText("Please Enter Your Current Address");
+		
+		Label lblAddress = new Label(shell, SWT.NONE);
+		lblAddress.setBounds(10, 65, 70, 20);
+		lblAddress.setText("Address:");
+		
+		Button btnFindNearestCooling = new Button(shell, SWT.NONE);
+		btnFindNearestCooling.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+		        if(address.getText().equals("")) {
+			        MessageBox dialog = new MessageBox(shell, SWT.ICON_ERROR | SWT.OK);
+					dialog.setText("ERROR");
+					dialog.setMessage("Please enter a valid address");
+					dialog.open();
+		        }
+		        else {
+					Label lblSearching = new Label(shell, SWT.NONE);
+					lblSearching.setBounds(10, 134, 469, 20);
+					lblSearching.setText("Searching........");
+		        }
+			}
+		});
+		btnFindNearestCooling.setBounds(10, 103, 469, 30);
+		btnFindNearestCooling.setText("Find Nearest Cooling Center");
+		
+		Button btnBack = new Button(shell, SWT.NONE);
+		btnBack.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				shell.dispose();
+				MainWindow MainWindow = new MainWindow();
+				MainWindow.open();
+			}
+		});
+		btnBack.setBounds(10, 10, 70, 30);
+		btnBack.setText("Back");
+		
+
+
+	}
+}
