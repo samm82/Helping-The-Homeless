2e96eb8c89cad34c29e0ba4ebe6b1394a8ba02ca
unknown <marin@mcmaster.ca>
2019-04-12 22:43:08 -0400

    added try catch to cath gui not loading

--- src/control/Master.java
+++ src/control/Master.java
@@ -18,8 +18,43 @@ public class Master {
 		try {
 		MainWindow window = new MainWindow();
 		window.open();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}		
-	}	
+		} 
+		catch (java.lang.UnsatisfiedLinkError e) {
+			System.out.println("The GUI Was unable to open");
+			System.out.println("This could be due to a variety of reasons,");
+			System.out.println("But the primary reason may be due to the SWT jar reference");
+			System.out.println("In order to fix this follow these steps (This was aproved by Andy):");
+			System.out.println("1. double click on 'Referenced Libraries' in the Package Explorer");
+			System.out.println("2. Ensure the version of the jar file matches your operating system");
+			System.out.println("win = windows, linux = linux, mac = osx");
+			System.out.println("3. if the jar does not match your operating system follow these steps:");
+			System.out.println("3A. right click on the jar in 'Referenced Libraries'");
+			System.out.println("3B. Under the 'Build Path' tab click on 'Remove from build path'");
+			System.out.println("3C. in the 'lib' folder in the package explorer right click on the jar for your operating system");
+			System.out.println("3D. Under the 'Build Path' tab click on 'add to build path'");
+			System.out.println("4. once the above steps have been completed, run the program again.");
+			System.out.println("Thank you for taking the time to follow these steps!");
+			System.out.println("SWT requires different jar files for each operating system\nso this solution is the best we can do to try and have the gui work for everyone at the moment since \ndynamically loading the jars at runtime did not work.");
+		}	
+		catch (java.lang.Error e) {
+			System.out.println("The GUI Was unable to open");
+			System.out.println("This could be due to a variety of reasons,");
+			System.out.println("But the primary reason may be due to the SWT jar reference");
+			System.out.println("In order to fix this follow these steps (This was aproved by Andy):");
+			System.out.println("1. double click on 'Referenced Libraries' in the Package Explorer");
+			System.out.println("2. Ensure the version of the jar file matches your operating system");
+			System.out.println("win = windows, linux = linux, mac = osx");
+			System.out.println("3. if the jar does not match your operating system follow these steps:");
+			System.out.println("3A. right click on the jar in 'Referenced Libraries'");
+			System.out.println("3B. Under the 'Build Path' tab click on 'Remove from build path'");
+			System.out.println("3C. in the 'lib' folder in the package explorer right click on the jar for your operating system");
+			System.out.println("3D. Under the 'Build Path' tab click on 'add to build path'");
+			System.out.println("4. once the above steps have been completed, run the program again.");
+			System.out.println("Thank you for taking the time to follow these steps!");
+			System.out.println("SWT requires different jar files for each operating system\nso this solution is the best we can do to try and have the gui work for everyone at the moment since \ndynamically loading the jars at runtime did not work.");
+		}	
+		catch (Exception e) {
+			System.out.println("There was an error, please try again");
+		}
+	}
 }
